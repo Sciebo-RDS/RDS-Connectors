88733f5af074b64c8ad5b387fb9c1d9109282a70
Stefan Jakobsson <stefan.jakobsson@snd.gu.se>
2023-01-26 15:31:40 +0100

    Add some comments/planning

--- doris-rds/src/DorisScieboRdsConnector/Controllers/ConnectorController.cs
+++ doris-rds/src/DorisScieboRdsConnector/Controllers/ConnectorController.cs
@@ -26,20 +26,24 @@ public class ConnectorController : ControllerBase
     {
         // Generate project identifier
         // Create storage space/path/bucket based on project identifier
+        // Do we need tagging or something similar when creating the bucket?
+
 
         logger.LogInformation($"CreateProject (POST /metadata/project), userId: {request.UserId}, metadata: {request.Metadata}");
 
         return Ok(new
         {
-            ProjectId = Guid.NewGuid().ToString(),
-            Metadata = new JsonObject()
+            ProjectId = Guid.NewGuid().ToString(), // Is UUID the right choice here? Or something more human readable?
+            Metadata = new JsonObject() // How is this used?
         });
     }
 
     [HttpPatch("metadata/project/{projectId}")]
     public IActionResult UpdateMetadata(string projectId, PortUserNameWithMetadata request)
     {
-        // Do nothing? Can we remove this endpoint?
+        // Can we create a profile with only a project label? We can use the label in Doris when selecting manifest.
+        // Store the metadata somewhere, so that we can access project label later when generating the Doris
+        // RO-Crate manifest? Or can we fetch it from Sciebo RDS somehow?
 
         logger.LogInformation($"UpdateMetadata (PATCH /metadata/project/{projectId}), userId: {request.UserId}, metadata: {request.Metadata}");
 
@@ -67,7 +71,7 @@ public class ConnectorController : ControllerBase
     public NoContentResult PublishProject(string projectId, PortUserName request)
     {
         // Check that project has been created in storage
-        // Generate RO-Crate manifest with file metadata from storage
+        // Generate RO-Crate manifest with file metadata from storage and possibly project label from Describo manifest
         // Post manifest to index server at SND
 
         logger.LogInformation($"PublishProject (PUT /metadata/project/{projectId}), userId: {request.UserId}");
