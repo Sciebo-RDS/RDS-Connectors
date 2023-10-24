961e45fe94ab03f60a06c115e47ec46bf02bff54
Stefan Jakobsson <stefan.jakobsson@snd.gu.se>
2023-10-24 13:52:47 +0200

    Temporarily disable httpClient SSL cert validation

--- doris-rds/src/DorisScieboRdsConnector/Program.cs
+++ doris-rds/src/DorisScieboRdsConnector/Program.cs
@@ -8,14 +8,27 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using System.Net.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddHttpClient<IDorisService, DorisService>();
-builder.Services.AddHttpClient<IScieboRdsService, ScieboRdsService>();
-builder.Services.AddHttpClient<IStorageService, NextCloudStorageService>();
+builder.Services.AddHttpClient<IScieboRdsService, ScieboRdsService>().ConfigureHttpMessageHandlerBuilder((c) =>
+     new HttpClientHandler()
+     {
+         ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+     }
+   );
+
+builder.Services.AddHttpClient<IStorageService, NextCloudStorageService>().ConfigureHttpMessageHandlerBuilder((c) =>
+     new HttpClientHandler()
+     {
+         ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+     }
+   );
+
 builder.Services.AddHttpClient<OcsApiClient>();
 
 builder.Services.AddOptions<DorisConfiguration>()
