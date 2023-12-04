29658348c0c78aff827ab57601d17da463181b15
Stefan Jakobsson <stefan.jakobsson@snd.gu.se>
2023-12-04 10:08:42 +0100

    Upgrade to .NET 8

--- doris-rds/src/DorisScieboRdsConnector/Program.cs
+++ doris-rds/src/DorisScieboRdsConnector/Program.cs
@@ -9,24 +9,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
-using Polly;
-using Polly.Extensions.Http;
-using System;
-using System.Net.Http;
-
-static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
-{
-    return HttpPolicyExtensions
-        .HandleTransientHttpError()
-        .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
-}
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddHttpClient<IDorisService, DorisService>()
-    .AddPolicyHandler(GetRetryPolicy());
+    .AddStandardResilienceHandler();
 builder.Services.AddHttpClient<IScieboRdsService, ScieboRdsService>();
 builder.Services.AddHttpClient<IStorageService, NextCloudStorageService>();
 builder.Services.AddHttpClient<OcsApiClient>();
